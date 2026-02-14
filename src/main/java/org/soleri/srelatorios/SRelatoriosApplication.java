79ee3874b92ac79faa4905453bc71c06f87ff69d
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2026-02-13 22:36:10 -0300

    first commit

---
+++ src/main/java/org/soleri/srelatorios/SRelatoriosApplication.java
@@ -0,0 +1,13 @@
+package org.soleri.srelatorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SRelatoriosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SRelatoriosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/soleri/srelatorios/SRelatoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soleri.srelatorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SRelatoriosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
