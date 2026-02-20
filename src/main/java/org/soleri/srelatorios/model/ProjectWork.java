412f5553e286373c42b0beae5eba8fae6b001a20
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2026-02-19 22:46:51 -0300

    fix: currect class

--- src/main/java/org/soleri/srelatorios/model/ProjectWork.java
+++ src/main/java/org/soleri/srelatorios/model/ProjectWork.java
@@ -1,18 +1,36 @@
 package org.soleri.srelatorios.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
-public class ProjectWork {
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProjectWork implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String code;
-    private String Client;
+
+    @Column(nullable = false)
+    private String client;
+
+    @Column(columnDefinition = "TEXT")
     private String description;
 }
