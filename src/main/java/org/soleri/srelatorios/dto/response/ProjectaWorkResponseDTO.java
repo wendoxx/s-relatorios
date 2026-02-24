5a56f89cadd5ca588e99355171d4bcc20bd3b812
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2026-02-23 22:14:25 -0300

    chore: rename file

--- src/main/java/org/soleri/srelatorios/dto/response/ProjectaWorkResponseDTO.java
+++
@@ -1,6 +0,0 @@
-package org.soleri.srelatorios.dto.response;
-
-import java.util.UUID;
-
-public record ProjectaWorkResponseDTO(UUID id, String name, String code, String client, String description) {
-}
