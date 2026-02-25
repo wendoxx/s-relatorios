2e8cb049a3ac9eb838d725ac2c27838df86793af
Wendel Silva <wendeldasilvamartins1952@gmail.com>
2026-02-24 22:44:34 -0300

    feat: add employee model

---
+++ src/main/java/org/soleri/srelatorios/model/Employee.java
@@ -0,0 +1,30 @@
+package org.soleri.srelatorios.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.soleri.srelatorios.utils.enums.EmployeeRole;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tb_employee")
+public class Employee implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private EmployeeRole role;
+}
